bc840a45dd851d7070f78e5696bed25803b6ea13
Adlet Kurentaev <kurentaev@gmail.com>
2022-09-20 13:54:19 +0600

    Изменен файл settings.py

--- source/todo_list/settings.py
+++ source/todo_list/settings.py
@@ -118,8 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, "static/css"),
-   os.path.join(BASE_DIR, "static/media"),
+   os.path.join(BASE_DIR / 'webapp/static/css'),
 ]
 
 # Default primary key field type

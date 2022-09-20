944e928f962c28185acf1d5f19f8e5b45d8c462a
Adlet Kurentaev <kurentaev@gmail.com>
2022-09-20 13:40:13 +0600

    Настроен файл urls.py

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -2,12 +2,8 @@ from django.urls import path
 
 from webapp.views.articles import add_view
 from webapp.views.base import index_view
-from webapp.views.articles import detail_view
-
 
 urlpatterns = [
     path('', index_view),
-    path('articles/add/', add_view),
-    path('articles/', detail_view)
+    path('todo_list/add/', add_view)
 ]
-

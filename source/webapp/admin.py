d74307a5218ec956e90cc7d9771dee5de60c1b31
Adlet Kurentaev <kurentaev@gmail.com>
2022-09-20 13:38:06 +0600

    Настроен файл admin.py

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import TasksList
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description', 'status', 'deadline']
+    list_filter = ['id', 'description', 'status', 'deadline']
+    search_fields = ['description', 'status']
+    fields = ['id', 'description', 'status', 'deadline']
+    readonly_fields = ['id', 'deadline']
+
+
+admin.site.register(TasksList, TaskAdmin)
+

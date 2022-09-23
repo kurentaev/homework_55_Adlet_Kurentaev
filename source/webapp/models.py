2675b13e996d4025c37e90e324c50e6c4c992431
Adlet Kurentaev <kurentaev@gmail.com>
2022-09-23 14:45:25 +0600

    Внесены изменения в models.py

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -4,9 +4,10 @@ from django.db import models
 class TasksList(models.Model):
     choices = [('new', 'Новая'), ('in_process', 'В процессе'), ('done', 'Сделано')]
 
-    description = models.TextField(max_length=3000, null=False, blank=False, default='None', verbose_name='Описание')
+    title = models.TextField(max_length=1000, null=False, blank=False, default='None', verbose_name='Задача')
     status = models.CharField(max_length=200, choices=choices, default='new', verbose_name='Статус')
     deadline = models.DateField(null=True, blank=True, verbose_name='Срок выполнения')
+    description = models.TextField(max_length=3000, null=True, blank=True, verbose_name='Описание')
 
     def __str__(self):
-        return f"{self.description} - {self.status} - {self.deadline}"
+        return f"{self.title} - {self.status} - {self.deadline}"

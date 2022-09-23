e8d83e60ab8fd08e14ce28bdba5c736cf120af7d
Adlet Kurentaev <kurentaev@gmail.com>
2022-09-23 14:47:06 +0600

    Внесены изменения в urls.py

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from webapp.views.articles import add_view
+from webapp.views.tasks import add_view
 from webapp.views.base import index_view
+from webapp.views.tasks import task_view
+from webapp.views.tasks import delete_view
 
 urlpatterns = [
-    path('', index_view),
-    path('todo_list/add/', add_view)
+    path('', index_view, name='index'),
+    path('todo_list/add/', add_view, name='todo_add'),
+    path('todo_list/', index_view),
+    path('todo_list/<int:pk>', task_view, name='todo_detail'),
+    path('delete_task/<int:pk>', delete_view, name='delete')
 ]
